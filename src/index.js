fafecf6dca985eb5e6f7d473d506da80da364f76
Elizabeth <elizabethtreahy@gmail.com>
2022-03-21 16:29:34 -0400

    DOM Content Loaded javascript

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+document.querySelector('#create-task-form').addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  let li = document.createElement('li')
+  let val = document.querySelector('#new-task-description').value
+  li.textContent = val
+  document.querySelector('#tasks').append(li)
+});
+
